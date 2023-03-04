e55c475707d491fd67b0a23c86e5a6ed27a150cc
PacoAlexM <pako_ale_martell@hotmail.com>
2023-03-04 17:49:46 -0600

    Mi sexto commit

---
+++ app/ws/uploadFiles.php
@@ -0,0 +1,19 @@
+<?php
+
+require "SimpleAjaxUploader/Uploader.php";
+
+$simpleAjaxUploader = new FileUpload("inputUploadFilesSimpleAjaxUploader");
+
+$simpleAjaxUploader->sizeLimit = ((/*byte*/ 1 * /*bytes*/ 1024) /* = kilobyte*/ * /*kilobytes*/ 1024) /* = megabyte*/ * /*megabytes*/ 10;
+
+$result = $simpleAjaxUploader->handleUpload(dirname(__FILE__) . "/files");
+
+if (!$result) {
+	exit(json_encode(array("success" => false, "msg" => $simpleAjaxUploader->getErrorMsg())));
+}
+
+if (!unlink($simpleAjaxUploader->getSavedFile())) {
+	exit(json_encode(array("success" => false, "msg" => "No fue posible eliminar el archivo.")));
+}
+
+echo json_encode(array("success" => true, "file" => $simpleAjaxUploader->getSavedFile()));
